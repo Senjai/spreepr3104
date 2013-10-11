f79f78be7c0b9d6be147d80604b5392fb0645177
Senjai <r.crawfordwilson@gmail.com>
2013-10-11 03:50:27 +0000

    Bleugh

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Spreepr3104
       end
     end
 
-    config.assets.initialize_on_precompile false
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
